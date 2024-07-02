71b74910fc7978bfd6b240463f6529e62add57e2
duong <duong@gmail.com>
2024-07-02 23:24:15 +0700

    initial

---
+++ DATN_SmartHouse/SmartHouse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartHouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
